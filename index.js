f6f1791f8434c994ba4aad86384b1994c7dbd58e
Vinicius <viniciusmuniz4002@gmail.com>
2025-03-28 20:50:09 -0300

    Alterações para deploy serverless na Vercel

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import userRoutes from './routes/userRoutes.js';
-import productRoutes from './routes/productsRoutes.js'
+import userRoutes from '../routes/userRoutes.js';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
 
 // Middleware para CORS
 app.use(cors());
@@ -14,16 +12,8 @@ app.use(express.json());
 
 // Middleware para rotas de usuário
 app.use('/api', userRoutes);
-app.use('/usuario', userRoutes);
-app.use('/dados-fotos', productRoutes);
-app.use('/dados-produtos', productRoutes);
 
-// Rota de teste
-app.get('/test', (req, res) => {
-  res.send('API está funcionando!');
-});
-
-// Inicia o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+// Função para lidar com a requisição HTTP
+export default (req, res) => {
+  app(req, res);
+};
