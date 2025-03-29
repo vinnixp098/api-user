dcb4da17b159257f427e782b1fea0734ee7a5dde
Vinicius <viniciusmuniz4002@gmail.com>
2025-03-28 21:33:05 -0300

    Subindo melhorias #2

--- config/db.js
+++ config/db.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
+// Cria a pool de conexões
 const conexao = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,       // Endereço do host do banco de dados
+  user: process.env.DB_USER,       // Usuário do banco de dados
+  password: process.env.DB_PASSWORD, // Senha do banco de dados
+  database: process.env.DB_DATABASE  // Nome do banco de dados
 }).promise();
 
+// Exporta a conexão para uso em outras partes do projeto
 export default conexao;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import userRoutes from '../routes/userRoutes.js';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
@@ -14,6 +14,11 @@ app.use(express.json());
 app.use('/api', userRoutes);
 
 // Função para lidar com a requisição HTTP
+app.get('/test', (req, res) => {
+  res.send('API está funcionando!');
+});
+
+// Exportando a função de handler para o Vercel
 export default (req, res) => {
-  app(req, res);
+  app(req, res);  // Lida com a requisição e resposta via Express
 };
