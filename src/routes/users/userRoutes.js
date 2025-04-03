bad18c4e243b1713b68ab29952652f11b365843e
Vinicius <viniciusmuniz4002@gmail.com>
2025-04-02 23:22:43 -0300

    Subindo rota signInUserByToken com ajuste

--- src/routes/users/userRoutes.js
+++ src/routes/users/userRoutes.js
@@ -7,7 +7,7 @@ router.post('/ediUser', UserController.editUserById);
 router.post('/createUSer', UserController.createUSer);
 router.get('/getUser', UserController.getUserByUserName);
 router.post('/signInUser', UserController.signInUser);
-router.post('/signInUserByToken', UserController.signInUserByToken); // utiliza query params
+router.get('/signInUserByToken', UserController.signInUserByToken); // utiliza query params
 router.post('/deletar-usuario', UserController.deleteUserById); // utiliza query params
 
 export default router;
